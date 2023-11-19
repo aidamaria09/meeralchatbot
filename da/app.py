0fc2ecc21b2d7543febabd5b38f7a9eebb7d5f55
aidamaria09 <146584457+aidamaria09@users.noreply.github.com>
2023-11-19 09:18:13 +0200

    Update app.py

--- da/app.py
+++ da/app.py
@@ -10,7 +10,22 @@ import os
 import base64
 import base64
 
- 
+ def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+
+
 
 def main():
     my_secret_value = st.secrets["OPENAI_API_KEY"]
@@ -24,7 +39,7 @@ def main():
     )
    
     
-    
+     add_bg_from_local('Struct.png') 
     header_container = st.container()
 
     
